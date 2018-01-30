6a401374c703a1f3b007118a0b192000ade5eaa6
Shin Dongjin <djshin2000@gmail.com>
2018-01-30 11:42:15 +0900

    blog app 을 installed_app에 추가, Post모델 생성 및 등록

---
+++ django/blog/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from . import views # 상대 경로 import
+# from blog import views <- 절대 경로 import
+
+urlpatterns = [
+    path('', views.post_list)
+]
--- django/blog/views.py
+++ django/blog/views.py
@@ -1,3 +1,16 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def post_list(request):
+    # 1. 브라우저에서 요청
+    # 2. 요청이 runserver로 실행중인 서버에 도착
+    # 3. runserver는 요청을 Django code로 전달
+    # 4. Django code중 config.urls모듈이 해당 요청을 받음
+    # 5. config.urls 모듈은 받은 요청의 URL과 일치하는 blog.urls모듈로 전달
+    # 6. blog.urls모듈은 받은 요청의 URL과 일치하는 패턴이 있는지 검사
+    # 7. 있다면 일치하는 패턴과 열결된 함수(view)를 실행
+    # 8. 함수의 실행함 결과(리턴값)을 브라우저로 다시 전달
+
+    # Http 프로토토콜로 텍스트 데이터 응답을 반환
+    return HttpResponse('<html><body><h1>Post list</h1><p>post 목록을 보여줄 예정입니다.</p></body></html>')
